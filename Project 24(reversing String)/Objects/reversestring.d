76504af3983ce12bac28dfc96d28a8f07a74a6cb
Prajwal S Dalawai <prajwaldalawai123@gmail.com>
2024-05-23 20:16:16 +0530

    New changes

---
+++ Project 24(reversing String)/Objects/reversestring.d
@@ -0,0 +1,1 @@
+.\objects\reversestring.o: reverseString.s
--- Project-20(Ascending Order element)/ascending.s
+++ Project-20(Ascending Order element)/ascending.s
@@ -1,22 +1,38 @@
 ;ALP Program to Arrange elements of the array in Ascending Order.
-	AREA ascending,CODE,READONLY
-	MOV R5,#04
-L1	MOV R6,R5
-	MOV R0, #0X40000000
-L2	LDR R1,[R0]
-	LDR R2,[R0,#04]
-	CMP R1,R2
-	BLT SKIP
-	STR R1,[R0]
-	STR R1,[R0,#04]
-SKIP ADD R0,R0,#04
-	SUBS R6,#01
-	BNE L2
-	SUBS R5,#01
-	BNE L1
-	NOP
-	END
-
-	
-	
-		
+AREA ascending,CODE,READONLY
+		ENTRY
+		MOV R8,#04
+		LDR R2,=CVALUE
+		LDR R3,=DVALUE
+loop0  LDR R1,[R2],#4
+		STR R1,[R3],#4
+		SUBS R8,R8,#1
+		CMP R8,#0
+		BNE loop0
+start  MOV R5,#03
+        MOV R7,#0
+		LDR R1,=DVALUE
+loop   LDR R2,[R1],#4
+		LDR R3,[R1]
+		CMP R2,R3
+		BLT loop2
+		STR R2,[R1],#-4
+		STR R3,[R1]
+		MOV R7,#01
+		ADD R1,#04
+loop2  SUBS R5,R5,#01
+		CMP R5,#0
+		BNE loop 
+		CMP R7,#0
+		BNE start
+		NOP
+		NOP
+		NOP
+CVALUE DCD 0X44444444
+	   DCD 0X11111111
+	   DCD 0X33333333
+	   DCD 0X22222222
+  AREA data1,DATA,READWRITE
+DVALUE DCD 0X000
+	 
+	 END
